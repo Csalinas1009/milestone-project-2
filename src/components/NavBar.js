1689e063ba807ca99d8b98fed56baa63f900ffc0
Christian Salinas <ChristianSalinas1009@gmail.com>
2022-10-14 18:18:35 -0400

    added NavBar functionality

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+function NavBar() {
+    return <nav className="nav">
+        <a href="/" className="site-title">The Traveler</a>
+        <ul>
+            <li className="active">
+                <a href="About">About</a>
+            </li>
+        </ul>
+    </nav>
+}
+
+export default NavBar
