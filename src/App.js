81548f32bee8960a5fc0ba1f07f6a69277255883
Christian Salinas <ChristianSalinas1009@gmail.com>
2022-10-15 16:20:58 -0400

    added initial card component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import NavBar from './components/NavBar';
+import Card from './components/Card';
 
 function App() {
   return (
+      <><div>
+      <NavBar />
+    </div>
+      <div>
+        <Card />
+      </div></> 
     
-    <NavBar />
 
 )
 }
---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Card = () => {
+    return (
+        <div>
+            <h2>test</h2>
+        </div>
+    )
+}
+
+export default Card;
