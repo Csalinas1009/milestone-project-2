eb508a743cba472938a5d45af2d647dce1718ea5
Ian <naberbach@gmail.com>
2022-10-16 13:11:59 -0400

    added navbar and dropdown functionality

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import './styles/styles.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from './components/NavBar';
-import TravelCard from './components/TravelCard';
+import "./styles/styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import TravelCard from "./components/TravelCard";
+import React from "react";
+import Navigation from "./components/NavBar";
 
 function App() {
   return (
-      <><div className='NavBar'>
-      <NavBar />
-    </div><div className='TravelCardDiv'>
+      <><div className="NB">
+      <Navigation />
+    </div>
+    <div className="TravelCardDiv">
         <TravelCard />
       </div></>
-    
-
-)
+  );
 }
 
 export default App;
