6d4b8c4c78c671bcc527e3454b2decd99623ca1b
Christian Salinas <ChristianSalinas1009@gmail.com>
2022-10-17 16:24:28 -0400

    setup starting props for TravelCard

--- src/components/TravelCard.js
+++ src/components/TravelCard.js
@@ -4,18 +4,17 @@ import { CardGroup } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
-function TravelCard() {
+function TravelCard(props) {
   return (
    <CardGroup> 
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="https://placekitten.com/96/139" style={{height: '250px', width: '285px'}} />
+      <Card.Img variant="top" src={props.Image} style={{height: '250px', width: '285px'}} />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>{props.Title}</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+          {props.Text}
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="primary">Do something</Button>
       </Card.Body>
     </Card>
     </CardGroup>  
