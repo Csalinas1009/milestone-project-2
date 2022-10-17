53ef866d705c5f4e22434adc63117c425b3154fd
Christian Salinas <ChristianSalinas1009@gmail.com>
2022-10-17 18:34:19 -0400

    setup initial prop layout for TravelCard

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import './styles/styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from './components/NavBar';
 import TravelCard from './components/TravelCard';
+
+
 function App() {
+  //need to fetch API to pass from Parent to Child component.
   return (
     <><><><div>
       <Navigation />
     </div><div className='card-container'>
-        <TravelCard />
-        <TravelCard />
+        <TravelCard title='Colorado' />
+        <TravelCard title='Vegas'/>
       </div></><div className='card-container'>
-        <TravelCard />
-        <TravelCard />
+        <TravelCard title='Mexico' />
+        <TravelCard title='England' />
       </div></><div className='card-container'>
-        <TravelCard />
-        <TravelCard />
+        <TravelCard title='Germany' />
+        <TravelCard title='France'/>
       </div></>
   )
 
--- src/components/TravelCard.js
+++ src/components/TravelCard.js
@@ -5,19 +5,20 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 function TravelCard(props) {
+
   return (
-   <CardGroup> 
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={props.Image} style={{height: '250px', width: '285px'}} />
-      <Card.Body>
-        <Card.Title>{props.Title}</Card.Title>
-        <Card.Text>
-          {props.Text}
-        </Card.Text>
-        <Button variant="primary">Do something</Button>
-      </Card.Body>
-    </Card>
-    </CardGroup>  
+    <CardGroup>
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={props.image} style={{ height: '250px', width: '285px' }} />
+        <Card.Body>
+          <Card.Title>{props.title}</Card.Title>
+          <Card.Text>
+            {props.text}
+          </Card.Text>
+          <Button variant="primary">Do something</Button>
+        </Card.Body>
+      </Card>
+    </CardGroup>
   );
 }
 
