a10b4e816176b1c5f0470649c965510f4c8d8b2d
Ian <naberbach@gmail.com>
2022-10-18 19:58:30 -0400

    added styling and icons to buttons

---
+++ src/Pages/EditPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+
+function EditPost() {
+
+}
+
+export default EditPost;
