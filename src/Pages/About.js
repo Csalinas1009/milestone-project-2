a7274945213e01a1dde8325add354ae1609614e5
Ian <naberbach@gmail.com>
2022-10-23 11:35:29 -0400

    added about and stylings in css

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,17 +1,51 @@
-import React from 'react'
-import Navigation from '../components/NavBar'
-import '../styles/styles.css'
+import React from "react";
+import Navigation from "../components/NavBar";
+import "../styles/styles.css";
+import AboutIan from "../components/AboutIan";
+import AboutAar from "../components/AboutAar";
+import AboutGio from "../components/AboutGio";
+import AboutChris from "../components/AboutChris";
 
-
-function About(){
-
-    return(
-        <><div className='NB'>
-            <Navigation />
-        </div><div>
-                <h1>About</h1>
-            </div></>
-    )
+function About() {
+  return (
+    <>
+      <>
+        <div className="NB">
+          <Navigation></Navigation>
+        </div>
+        <>
+          <div className="AbPage">
+            <>
+              <div className="about">
+                <div className="title">
+                  <h2>About</h2>
+                </div>
+                <p>
+                  This is a sample app created by Ian Aberbach, Christian
+                  Salinas, Aaron Everette, and Giovanni Peres. The function of
+                  this app is to upload and post travel reviews. We utilized
+                  MERN to create this application.
+                </p>
+              </div>
+            </>
+            <div className="group">
+              <h1>Meet The Team</h1>
+              <h2>Frontend Developers</h2>
+              <div className="group1">
+                <AboutIan></AboutIan>
+                <AboutChris></AboutChris>
+              </div>
+              <h2>Backend Developers</h2>
+              <div className="group2">
+                <AboutGio></AboutGio>
+                <AboutAar></AboutAar>
+              </div>
+            </div>
+          </div>
+        </>
+      </>
+    </>
+  );
 }
 
 export default About;
