ac94f83606a9b9c16e6bcf88a77ffdf621199617
Christian Salinas <ChristianSalinas1009@gmail.com>
2022-10-23 03:28:37 -0400

    increased size on TravelCard

--- src/components/TravelCard.js
+++ src/components/TravelCard.js
@@ -13,7 +13,7 @@ function TravelCard({pin, deletePin}) {
       {Array.from({ length: 1 }).map((_, idx) => (
         <Col>
           <Card>
-            <Card.Img variant="top" src={pin.imgURL} style={{ height: "250px", width: "275px" }}/>
+            <Card.Img variant="top" src={pin.imgURL} style={{ height: "325px", width: "400px" }}/>
             <Card.Body>
               <Card.Title>{pin.title}</Card.Title>
               <Card.Text>
