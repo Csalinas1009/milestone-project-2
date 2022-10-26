8fdeaf0e0b64e62cf7e5a9782abf039c2b6984aa
Csalinas1009 <Christiansalinas1009@gmail.com>
2022-10-26 09:45:38 -0400

    Cleaned up code format on all files.

--- src/App.js
+++ src/App.js
@@ -1,56 +1,52 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import './styles/styles.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Axios from 'axios';
-import TravelCard from './components/TravelCard'
-import Navigation from './components/NavBar';
-
+import React from "react";
+import { useState, useEffect } from "react";
+import "./styles/styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Axios from "axios";
+import TravelCard from "./components/TravelCard";
+import Navigation from "./components/NavBar";
 
 function App() {
   //useState hook for setting up our cards
   const [pins, setPins] = useState([]);
 
-
   useEffect(() => {
     //get request from back-end database
-    Axios.get('https://milestone2-travlr.herokuapp.com/pin').then((response) => {
-      setPins(response.data)
-    })
+    Axios.get("https://milestone2-travlr.herokuapp.com/pin").then(
+      (response) => {
+        setPins(response.data);
+      }
+    );
   }, []);
 
   const deletePin = async (id) => {
     try {
-      const res = await Axios.delete(`https://milestone2-travlr.herokuapp.com/pin/${id}`);
+      const res = await Axios.delete(
+        `https://milestone2-travlr.herokuapp.com/pin/${id}`
+      );
       console.log({ res }, res.response.data);
-     } catch (error) { 
-      alert('Post Deleted!');
+    } catch (error) {
+      alert("Post Deleted!");
       window.location.reload();
-      
-  }
-}
+    }
+  };
 
   //render to page
   return (
-    <div className='NB'>
+    <div className="NB">
       <Navigation />
       <div>
-        {pins && pins.map((pin) => {
-          return (
-            <div className='card-container'>
-              <TravelCard
-                pin={pin}
-                deletePin={deletePin} 
-              />
-            </div>
-          );
-        })}
+        {pins &&
+          pins.map((pin) => {
+            return (
+              <div className="card-container">
+                <TravelCard pin={pin} deletePin={deletePin} />
+              </div>
+            );
+          })}
       </div>
     </div>
-  )
-
-
-
+  );
 }
 
 export default App;
--- src/components/TravelCard.js
+++ src/components/TravelCard.js
@@ -1,54 +1,86 @@
 import React from "react";
 import { useState } from "react";
-import EditForm from './EditForm';
+import EditForm from "./EditForm";
 import "../styles/styles.css";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenSquare, faTrashCan } from "@fortawesome/free-solid-svg-icons";
 
-
-function TravelCard({pin, deletePin}) {
-  
+function TravelCard({ pin, deletePin }) {
   const [showForm, setShowForm] = useState(false);
 
-
-  return ( 
-    <Row xs='auto' className="g-5">
+  return (
+    <Row xs="auto" className="g-5">
       {Array.from({ length: 1 }).map((_, idx) => (
-        <><Col>
-          <Card>
-            <Card.Img variant="top" src={pin.imgURL} style={{height: '450px%', width: '450px'}} />
-            <Card.Body>
-              <Card.Title>{pin.title}</Card.Title>
-              <Card.Text style={{ color: 'black' }}>
-                {pin.description}
-              </Card.Text>
-            </Card.Body>
-            <div className="btns">
-              <div className="editBtn">
-                <Button variant="primary" onClick={() => setShowForm(true)} style={{position: 'relative', bottom: '320px', right: '53px'}}>
-                  <FontAwesomeIcon icon={faPenSquare}></FontAwesomeIcon>
-                </Button>
+        <>
+          <Col>
+            <Card>
+              <Card.Img
+                variant="top"
+                src={pin.imgURL}
+                style={{ height: "450px%", width: "450px" }}
+              />
+              <Card.Body>
+                <Card.Title>{pin.title}</Card.Title>
+                <Card.Text style={{ color: "black" }}>
+                  {pin.description}
+                </Card.Text>
+              </Card.Body>
+              <div className="btns">
+                <div className="editBtn">
+                  <Button
+                    variant="primary"
+                    onClick={() => setShowForm(true)}
+                    style={{
+                      position: "relative",
+                      bottom: "320px",
+                      right: "53px",
+                    }}
+                  >
+                    <FontAwesomeIcon icon={faPenSquare}></FontAwesomeIcon>
+                  </Button>
+                </div>
+                <div className="delBtn">
+                  <Button
+                    variant="primary"
+                    onClick={() => deletePin(pin._id)}
+                    style={{
+                      position: "relative",
+                      bottom: "365px",
+                      right: "100px",
+                    }}
+                  >
+                    <FontAwesomeIcon icon={faTrashCan}></FontAwesomeIcon>
+                  </Button>
+                </div>
               </div>
-              <div className="delBtn">
-                <Button variant='primary' onClick={() => deletePin(pin._id)} style={{position: 'relative', bottom: '365px', right: '100px'}}>
-                  <FontAwesomeIcon icon={faTrashCan}></FontAwesomeIcon>
+            </Card>
+            <div className="edit-form">
+              {showForm && <EditForm pin={pin} setShowForm={setShowForm} />}
+              {showForm && (
+                <Button
+                  style={{
+                    position: "relative",
+                    left: "140px",
+                    bottom: "35px",
+                  }}
+                  type="button"
+                  class="btn-close"
+                  aria-label="Close"
+                  onClick={() => setShowForm(false)}
+                >
+                  X
                 </Button>
-              </div>
+              )}
             </div>
-          </Card>
-          <div className="edit-form">
-            {showForm && <EditForm pin={pin} setShowForm={setShowForm} />}
-            {showForm && <Button style={{position: 'relative', left: '140px', bottom: '35px'}} type="button" class='btn-close' aria-label="Close" onClick={() => setShowForm(false)}>X</Button>}
-          </div>
-        </Col></>
+          </Col>
+        </>
       ))}
-    </Row>  
+    </Row>
   );
 }
-
 
 export default TravelCard;
